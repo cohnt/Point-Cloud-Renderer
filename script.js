fa1d19153a653fe5c105374b37968244620c9692
Thomas Cohn <=>
2017-10-16 13:56:53 -0400

    Putting in the constants and global variables needed for the rendering system.

--- script.js
+++ script.js
@@ -4,6 +4,14 @@
 
 var canvasWidth = 750;
 var canvasHeight = 750;
+var zoomPowerConstant = 1.1; //This is used when calculating the zoom factor when scrolling.
+var mouseWheelCalibrationConstant = 53; //This is the value given when the mouse is scrolled one notch.
+var axisColors = ["#ff0000", "#00cc00", "#0000ff"]; //The colors of the axes shown about global zero.
+var dragRotatingConstant = 1/100; //This constant slows down the rate that dragging rotates the graph.
+var dragPanningConstant = 1/40; //This constant slows down the rate that dragging pans the graph.
+var rotateCheckButtonSpeed = 25; //How often the program checks if the rotate button is still pressed, in milliseconds.
+var rotateDegreesPerTick = 1.5; //How many degrees the view rotates per tick.
+var mouseDeltasToKeep = 8; //How many of the last mouse movements to keep recorded for panning/rotating.
 
 ///////////////////////////////////////////
 /// GLOBAL VARIABLES
@@ -11,6 +19,9 @@ var canvasHeight = 750;
 
 var html = {};
 var pointCloud = [];
+var viewVector = [];
+var viewBasis = [];
+var cameraLocation = [];
 
 ///////////////////////////////////////////
 /// CLASSES
@@ -26,6 +37,8 @@ function setup() {
 	html.canvas = document.getElementById("display");
 	html.pcInput = document.getElementById("pcInput");
 	html.pcInputButton = document.getElementById("pcInputButton");
+	html.startButton = document.getElementById("start");
+	html.stopButton = document.getElementById("stop");
 
 	html.canvas.setAttribute("width", String(canvasWidth) + "px");
 	html.canvas.setAttribute("height", String(canvasHeight) + "px");
