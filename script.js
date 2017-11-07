43444dec701a2e809b0f8fd0f3511c85864e763d
Thomas Cohn <=>
2017-11-06 20:54:14 -0500

    Canvas transforms, commenting.

--- script.js
+++ script.js
@@ -2,13 +2,20 @@
 /// CONSTANTS
 ///////////////////////////////////////////
 
-
+var defaultTransformMatrix = [[1, 0, 0, 0], //The default transformation matrix.
+                              [0, 1, 0, 0],
+                              [0, 0, 1, 0],
+                              [0, 0, 0, 1]];
+var axisColors = ["#ff0000", "#00ff00", "#0000ff"]; //The colors of the x, y, and z axes (respectively).
+var canvasDimensions = [null, null]; //Populated in setup()
 
 ///////////////////////////////////////////
 /// GLOBAL VARIABLES
 ///////////////////////////////////////////
 
 var html = {}; //An object containing every html element on the page.
+var context; //The to-be-created 2D canvas context.
+var currentTransform; //The current matrix transformation to map a point in the 3D real space to the camera perspective.
 
 ///////////////////////////////////////////
 /// CLASSES
@@ -21,14 +28,30 @@ var html = {}; //An object containing every html element on the page.
 ///////////////////////////////////////////
 
 function setup() {
-	html.canvas = document.getElementById("screen");
-	html.currentTransform = [];
+	html.canvas = document.getElementById("screen"); //<canvas id="screen">
+	html.currentTransform = []; //Load in the entire table which displays the current transform.
 	for(var i=0; i<4; ++i) {
 		html.currentTransform.push([]);
 		for(var j=0; j<4; ++j) {
-			html.currentTransform[i].push(document.getElementById("r"+i+"c"+j));
+			html.currentTransform[i].push(document.getElementById("r"+i+"c"+j)); //Matrix element in row i, column j has id ricj.
 		}
 	}
+
+	//The raw output of getAttribute is "####px", so we need to shave off the px and parse to a number.
+	canvasDimensions[0] = Number(html.canvas.getAttribute("width").slice(0,-2));
+	canvasDimensions[1] = Number(html.canvas.getAttribute("height").slice(0,-2));
+
+	context = html.canvas.getContext("2d"); //Create a 2D context.
+	context.transform(1, 0, 0, 1, canvasDimensions[0]/2, canvasDimensions[1]/2); //Put the origin in the center of the canvas.
+	context.transform(1, 0, 0, -1, 0, 0); //Flip it so y+ is up.
+
+	loadDefaults();
+}
+function loadDefaults() {
+	currentTransform = defaultTransformMatrix.slice();
+}
+function drawAxes() {
+
 }
 
 ///////////////////////////////////////////
